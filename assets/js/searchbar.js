fb5b03b879aa3823ff4808c3e0121bd0fe0e33ff
shayanb1 <your.github.email@example.com>
2025-04-23 14:31:06 -0700

    cool shit

---
+++ assets/js/searchbar.js
@@ -0,0 +1,15 @@
+
+//Storing Data
+//localStorage.setItem('username', 'pika43');
+
+//Retrieving Data
+//let username = localStorage.getItem('username');
+//console.log(username);  // Output: pika43
+
+//Removing Data
+//localStorage.removeItem('username');
+
+
+//Clearing Data
+//localStorage.clear();
+
